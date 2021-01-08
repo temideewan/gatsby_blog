0e8f90b6b4abf63b6f09c60527c89a985aa15de9
temidayo omoyajowo <temideewan@gmail.com>
2021-01-08 14:05:23 +0100

    added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,51 @@
-
-
 module.exports = {
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
-      options:{
+      options: {
         name: `pages`,
-        path: `${__dirname}/src/pages`
+        path: `${__dirname}/src/pages`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
-      options:{
+      options: {
         name: `posts`,
-        path: `${__dirname}/src/posts`
+        path: `${__dirname}/src/posts`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
-      options:{
+      options: {
         name: `images`,
-        path: `${__dirname}/src/images`
+        path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-plugin-sharp`, `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-mdx`,
-      options:{
-        extensions: [ `.md`, `.mdx`],
-        gatsbyRemarkPlugins: [{
-          resolve: `gatsby-remark-images`,
-          options: {
-            maxWidth: 1200,
-
+      options: {
+        extensions: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
           },
-        },
-      ],
+        ],
       },
     },
-
+    {
+      resolve: `gatsby-plugin-googlefonts`,
+      options:{
+        fonts:[
+          `roboto mono`,
+          `muli\:400,400i,700,700i`,
+        ],
+        display: "swap",
+      },
+    },
   ],
 }
