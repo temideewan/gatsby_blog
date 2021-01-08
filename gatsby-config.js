477f05db8919bbab3afe51ee19b385236a25b8ac
temidayo omoyajowo <temideewan@gmail.com>
2021-01-08 14:14:36 +0100

    configure google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,11 +39,8 @@ module.exports = {
     },
     {
       resolve: `gatsby-plugin-googlefonts`,
-      options:{
-        fonts:[
-          `roboto mono`,
-          `muli\:400,400i,700,700i`,
-        ],
+      options: {
+        fonts: [`roboto mono`, `muli\:400,400i,700,700i`],
         display: "swap",
       },
     },
